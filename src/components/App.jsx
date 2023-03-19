3ba49401ac9cb8837a3a093f4edd926f3454d799
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-03-19 15:28:53 +0200

    files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistic } from './Statistics';
+import { Notification } from './Notification';
+import { Section } from './Section/section';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  addFeedBack = event => {
+    this.setState(prevState => {
+      return {
+        [event.target.name]: prevState[event.target.name] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const add = this.state.good + this.state.neutral + this.state.bad;
+    return add;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.good) {
+      const positiv = Math.round(
+        this.state.good / (this.countTotalFeedback() / 100)
+      );
+      return positiv;
+    }
+    return 0;
+  };
+  render() {
+    return (
+      <div>
+        <Section title={'FeedbackOption'}>
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            addFeedback={this.addFeedBack}
+          ></FeedbackOptions>
+        </Section>
+        <Section title={'Statistic'}>
+          {this.countTotalFeedback() ? (
+            <Statistic
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              totalFeedback={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification massage={'There is no feedback'} />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,14 +1,19 @@
-export function Statistic({good, neutral, bad, totalFeedback, positivePercentage, title}) {
-    return <div>
-        <h2>{title}</h2>
-        <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-            <li>Totalfeedback: {totalFeedback}</li>
-            <li>Positive: {positivePercentage}%</li>
-        </ul>
+export function Statistic({
+  good,
+  neutral,
+  bad,
+  totalFeedback,
+  positivePercentage,
+}) {
+  return (
+    <div>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Totalfeedback: {totalFeedback}</li>
+        <li>Positive: {positivePercentage}%</li>
+      </ul>
     </div>
-   
-    
+  );
 }
